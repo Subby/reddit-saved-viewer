52809499e78070605e93876bfab9e3f1834d8d84
Denver Fernandes <sub367@gmail.com>
2020-03-11 14:11:11 +0000

    Add boxes to posts

--- src/components/SavedContentItem.js
+++ src/components/SavedContentItem.js
@@ -3,18 +3,21 @@ import React from 'react'
 
 const SavedContentItem = (props) => {
     return (
-        <div className="columns">
-            <div className="column is-2">
-                <img loading="lazy" src={props.thumbnail} alt="Thumbnail"/>
+        <div className="box">
+            <div className="media">
+                <figure className="media-left">
+                    <img loading="lazy" src={props.thumbnail} alt="Thumbnail"/>
+                </figure>
+                <div className="media-content">
+                    <div className="content">
+                        <h1><a href={props.url}>{props.title}</a></h1>
+                        <p><a href={'http://reddit.com/r/' + props.subreddit}>{props.subreddit}</a></p>
+                        <p><a href={'http://reddit.com/u/' + props.postedBy}>{props.postedBy}</a></p>
+                        <p>{props.postedDate}</p>
+                        <p>{props.nsfw}</p>
+                    </div>
+                </div>
             </div>
-            <div className="column">
-                <h1><a href={props.url}>{props.title}</a></h1>
-                <p><a href={'http://reddit.com/r/' + props.subreddit}>{props.subreddit}</a></p>
-                <p><a href={'http://reddit.com/u/' + props.postedBy}>{props.postedBy}</a></p>
-                <p>{props.postedDate}</p>
-                <p>{props.nsfw}</p>
-            </div>
-
         </div>
     )
 };
